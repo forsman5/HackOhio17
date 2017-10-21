b42096f9417c794605e3dd8a512c8f1e3e7f445f
Joseph Forsman <jrforsman@gmail.com>
2017-10-21 16:57:24 -0400

    added comments to constants file, denoting usage of each constant

--- Pi Scripts/constants.py
+++ Pi Scripts/constants.py
@@ -1,5 +1,14 @@
 #constants
+
+#pin marking where the button is connected to
 IN_PIN = 24
+
+#pin that controls the checking on startup
+    # if this is true, then after finishing exectuion, the py will NOT shut down
+    # otherwise it will shut down
 SWITCH_PIN = 23
+
+#constants to control actions on button pressing - for in case of inversion
+    #also allows code to be easier to understand
 BUTTON_PRESSED = False
 BUTTON_UNPRESSED = True
